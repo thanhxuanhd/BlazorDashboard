d61aff835e1d9fc5890c08fe2032b024dafe0d14
Nguyễn Thanh Xuân <thanhxuanhd007@gmail.com>
2022-05-07 11:44:05 +0700

    Add Create User Form

---
+++ BlazorDashboard/Models/UserViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDashboard.Models;
+
+public class UserViewModel
+{
+    [Required]
+    public string UserName { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    public string Language { get; set; }
+
+    [Required]
+    public List<UserRole> Roles { get; set; }
+}
+
+public class UserRole
+{
+
+    public string RoleId { get; set; }
+
+    public string RoleName { get; set; }
+
+    public bool IsChecked { get; set; }
+}
+
+public enum RoleEnum
+{
+    ROLE_ADMIN,
+    ROLE_MANAGER,
+    ROLE_USER
+}
